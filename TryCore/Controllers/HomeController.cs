4f55230ae8167c112199df8de2e6dc6381e33ae7
Aderbal Farias <aderbalfarias@hotmail.com>
2017-07-22 23:08:55 +0100

    clean the project

--- TryCore/Controllers/HomeController.cs
+++ TryCore/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace TryCore.Controllers
 {
@@ -15,15 +11,11 @@ namespace TryCore.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "xpto";
-
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
