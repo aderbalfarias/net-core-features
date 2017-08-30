0ce94f589b94e55945fc01279682dd3733548fa4
Aderbal Farias <aderbalfarias@hotmail.com>
2017-08-30 23:56:09 +0100

    update files

--- TryCore/Startup.cs
+++ TryCore/Startup.cs
@@ -23,8 +23,7 @@ namespace TryCore
         }
 
         public IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
