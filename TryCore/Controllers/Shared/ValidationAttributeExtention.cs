9134b34315750015f199ae69ed28fda1a921977a
Aderbal Farias <aderbalfarias@hotmail.com>
2017-09-05 23:58:24 +0100

    Update file

--- TryCore/Controllers/Shared/ValidationAttributeExtention.cs
+++ TryCore/Controllers/Shared/ValidationAttributeExtention.cs
@@ -4,17 +4,17 @@ namespace Gsys.Mvc.Models.Shared
 {
     public class ValidationAttributeExtention : ValidationAttribute
     {
-        private readonly int _espaco;
-        public ValidationAttributeExtention(int espaco)
+        private readonly int _espace;
+        public ValidationAttributeExtention(int espace)
             : base(@"{0} Contains spaces beyond allowed")
         {
-            _espaco = espaco;
+            _espace = espace;
         }
-
+        
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var valueAsSting = value?.ToString();
-            if (valueAsSting?.Split(' ').Length > _espaco)
+            if (valueAsSting?.Split(' ').Length > _espace)
             {
                 var errorMessage = FormatErrorMessage(validationContext.DisplayName);
                 return new ValidationResult(errorMessage);
