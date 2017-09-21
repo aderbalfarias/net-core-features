71119259772befe28ed62627c6c55eca471678c3
Aderbal Farias <aderbalfarias@hotmail.com>
2017-09-21 23:14:12 +0100

    Including new project

---
+++ AboutNetCore.Version1_0.DefaultTemplate/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AboutNetCore.Version1_0.DefaultTemplate.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
