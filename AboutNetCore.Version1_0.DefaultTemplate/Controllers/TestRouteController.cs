1217e6343fab30a14f16053f3f866f3e03929ddf
Aderbal Farias <aderbalfarias@hotmail.com>
2017-09-23 12:58:46 +0100

    About routes

---
+++ AboutNetCore.Version1_0.DefaultTemplate/Controllers/TestRouteController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AboutNetCore.Version1_0.DefaultTemplate.Controllers
+{
+    //[Route("[controller]")]
+    //[Route("[controller]/[action]")]
+    [Route("test")]
+
+    public class TestRouteController : Controller
+    {
+        [Route("")]
+        //[Route("[action]")]
+        public IActionResult Index()
+        {
+            ViewBag.Route = "Get Index";
+
+            return View();
+        }
+
+        [Route("xy")]
+        //[Route("[action]")]
+        public IActionResult GetXpto()
+        {
+            ViewBag.Route = "Get Index";
+
+            return View();
+        }
+    }
+}
